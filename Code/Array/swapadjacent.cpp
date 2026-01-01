2643859a19797ce346fcb44cd49df1af9e2912be
Amulya Kumar Maurya <akmewtriple7@gmail.com>
2026-01-01 23:00:54 +0530

    Initial commit - array problems

---
+++ Code/Array/swapadjacent.cpp
@@ -0,0 +1,34 @@
+//Code By: AMULYA MAURYA
+//2026-01-01
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+void swapAlternate(int arr[], int n) {
+    for (int i = 0; i < n - 1; i += 2) {
+        swap(arr[i], arr[i + 1]);
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+
+    int arr[]={1,2,3,4,5,6};
+
+    int n=sizeof(arr)/sizeof(arr[0]);
+    
+
+    swapAlternate(arr,n );
+    printArray(arr,n);
+
+    return 0;
+}
